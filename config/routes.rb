d16fa956547624463f2ec6edddd30483b8bad7ec
Zach Blass <zachary.blass@gmail.com>
2014-02-05 02:45:58 -0500

    3

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ WatermelonBust::Application.routes.draw do
 
   root "pages#home"
   get "about" => "pages#about"
-  get "media" => "#"
+  get "media" => "pages#media"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
