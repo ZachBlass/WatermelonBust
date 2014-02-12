f1358b2805e56add5730aa98ae0b97a7f3ca7e49
Zach Blass <zachary.blass@gmail.com>
2014-02-12 15:12:59 -0500

    #

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,7 @@ WatermelonBust::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
   get "media" => "pages#media"
-  get "inquiries" => "pages#inquiries"
-
-  resources :inquiries, :only => [:new, :create] do
-  get 'thank_you', :on => :collection
+  
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
